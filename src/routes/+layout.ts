765767896f978bf0a0bfed51daa5bfb5d791b9c8
amirhh00 <amirhh00@gmail.com>
2024-12-13 04:34:18 +0000

    Add @vercel/analytics integration

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,7 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
 export const prerender = true;
 export const ssr = false;
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
