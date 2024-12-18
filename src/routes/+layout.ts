eab326cf3642f93632f991396b07cdbc3157aa0d
amirhh00 <amirhh00@gmail.com>
2024-12-18 14:23:10 +0000

    Remove unused PUBLIC_IGNORE_ANALYTICS import

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,5 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
-import { PUBLIC_IGNORE_ANALYTICS } from '$env/static/public';
 export const prerender = true;
 export const ssr = false;
-if (!dev && !PUBLIC_IGNORE_ANALYTICS) injectAnalytics({ mode: dev ? 'development' : 'production' });
+if (!dev) injectAnalytics({ mode: dev ? 'development' : 'production' });
