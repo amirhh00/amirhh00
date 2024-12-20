d29c7ecd8f6f7e950401f98e167a927f46ac546f
amirhh00 <amirhh00@gmail.com>
2024-12-20 04:48:40 +0000

    Update resume PDF generation workflow and configuration for Deno

--- resumeToPdf/index.ts
+++ resumeToPdf/index.ts
@@ -7,7 +7,7 @@ await page.goto(BASE_URL + '/resume', {
 	waitUntil: 'networkidle2'
 });
 await page.pdf({
-	path: './static/resume.pdf',
+	path: '../static/resume.pdf',
 	format: 'A4',
 	margin: { top: '0cm', right: '0cm', bottom: '0cm', left: '0cm' }
 });
